43df888e65d6392aa5f790a2e303d692ce398848
Xytavious <143834355+Xytavious@users.noreply.github.com>
2024-10-30 17:05:11 +0000

    10/30/2024

---
+++ Project1.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+
+class Contact{
+    private:
+    string name;
+    string email;
+    string phone;
+    public:
+    Contact(string n,string e,string p){
+    this-> name = name; 
+    this-> email=email;
+    this-> phone=phone;
+    }
+    string get_name(){
+        return name;
+    }
+    string get_email(){
+        return email;
+    }
+    st get_phone(){
+        return phone;
+    }
+    void display(){
+        cout << "Name: "<<name << endl;
+        cout << "Email: "<< email<< endl;
+        cout << "Phone: "<< phone <<endl;
+    }
+
+}
+
+class main(){
+    private:
+    Contact
+    public:
+    void add_contact(Contact){
+
+    }
+}
